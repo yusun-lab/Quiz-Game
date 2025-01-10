edfdd60bb4d0b8c9d3dae971a55cffa40ad3ab16
yusun-lab <yu.sun@hyperisland.se>
2025-01-10 23:39:34 +0100

    Create DOM elements

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const API_URL = "https://opentdb.com/api.php?amount=10";
+let questions = [];
+let currentQuestionIndex = 0;
+let score = 0;
+
+// DOM Elements
+const questionEl = document.getElementById("question");
+const optionsEl = document.getElementById("options");
+const nextBtn = document.getElementById("next-btn");
+const restartBtn = document.getElementById("restart-btn");
+const questionCounterEl = document.getElementById("question-counter");
+const scoreEl = document.getElementById("score");
+
+
